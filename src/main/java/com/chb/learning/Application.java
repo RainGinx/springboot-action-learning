6ca4552c7ba68601a982cf518a6fa4399145a720
RainGinx <rainginx@gmail.com>
2021-04-26 16:17:18 +0800

    feature:add test file

---
+++ src/main/java/com/chb/learning/Application.java
@@ -0,0 +1,12 @@
+package com.chb.learning;
+
+import org.apache.catalina.core.ApplicationContext;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class,args);
+    }
+}
---
+++ src/main/java/com/chb/learning/controller/Test.java
@@ -0,0 +1,13 @@
+package com.chb.learning.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Test {
+
+    @GetMapping("/s")
+    public String returnString(){
+        return "Hello world";
+    }
+}
