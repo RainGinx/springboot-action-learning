5a80b2abd41e1353872c3afcff9c60937d3a403f
RainGinx <rainginx@gmail.com>
2021-05-27 10:42:16 +0800

    feature:添加nacos
    Signed-off-by: 蔡宏斌 <caihongbin@interlib.vip>

--- src/main/java/com/chb/learning/controller/Test.java
+++ src/main/java/com/chb/learning/controller/Test.java
@@ -1,13 +1,22 @@
 package com.chb.learning.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("user")
 public class Test {
 
-    @GetMapping("/s")
+    @PostMapping("/loginbyusername")
     public String returnString(){
         return "Hello world";
+    }
+
+    public static void main(String[] args) {
+        int result = get();
+        System.out.println(result);
+    }
+
+    private static Integer get(){
+        return null;
     }
 }
